547da1f658587f5efa6d7f8b10f59d76c89f11d0
Sanket Kharat <sanketkharat28@gmail.com>
2025-09-05 20:49:51 +0530

    stream the response fromm claude

--- be/src/index.ts
+++ be/src/index.ts
@@ -4,16 +4,14 @@ const { default: Anthropic } = require("@anthropic-ai/sdk");
 
 const anthropic = new Anthropic();
 async function main() {
-const msg = await anthropic.messages.create({
-    model: "claude-sonnet-4-20250514",
-    max_tokens: 1000,
-    temperature: 1,
-    messages: [{
-        role: "user",
-        content: "Write the code for a TODO application"
-        }]
+    anthropic.messages.stream({
+        messages: [{ role: 'user', content: "Create a simple todo app" }],
+        model: 'claude-sonnet-4-20250514',
+        max_tokens: 1024,
+    }).on('text', (text:unknown) => {
+        console.log(text);
     });
-    console.log(msg);
+
 }
 
 main();
