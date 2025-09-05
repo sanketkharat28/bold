b51fdd6c3928deb901c599ea6489f587ea8c91e9
Sanket Kharat <sanketkharat28@gmail.com>
2025-09-05 20:46:16 +0530

    use the claude sdk

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,2 +1,20 @@
+require("dotenv").config();
 
+const { default: Anthropic } = require("@anthropic-ai/sdk");
+
+const anthropic = new Anthropic();
+async function main() {
+const msg = await anthropic.messages.create({
+    model: "claude-sonnet-4-20250514",
+    max_tokens: 1000,
+    temperature: 1,
+    messages: [{
+        role: "user",
+        content: "Write the code for a TODO application"
+        }]
+    });
+    console.log(msg);
+}
+
+main();
 
